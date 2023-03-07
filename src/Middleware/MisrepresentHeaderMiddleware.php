5e357fa752a30bbb31930543bff9b99fdccdffc9
Nerahikada <hkshirorom@yahoo.co.jp>
2023-03-07 12:40:24 +0900

    (Not necessary but) reproduce some browser headers

--- src/Middleware/MisrepresentHeaderMiddleware.php
+++ src/Middleware/MisrepresentHeaderMiddleware.php
@@ -10,9 +10,15 @@ final class MisrepresentHeaderMiddleware extends RequestMiddleware
 {
     protected function mapRequest(RequestInterface $request): RequestInterface
     {
-        return $request->withHeader(
-            'User-Agent',
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
-        );
+        return $request
+            ->withHeader('Accept', 'application/json, text/plain, */*')
+            ->withHeader('Accept-Encoding', 'gzip, deflate, br')
+            ->withHeader('Accept-Language', 'ja,en-US;q=0.9,en;q=0.8')
+            ->withHeader('Origin', 'https://jp.mercari.com')
+            ->withHeader('Referer', 'https://jp.mercari.com/')
+            ->withHeader(
+                'User-Agent',
+                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
+            );
     }
 }
